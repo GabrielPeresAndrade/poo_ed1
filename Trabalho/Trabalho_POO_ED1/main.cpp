88503f215b320cd06775a891e22d2c09440b89be
Gabriel Peres <34287414+SirAargon@users.noreply.github.com>
2018-05-08 22:51:13 -0300

    Add files via upload

---
+++ Trabalho/Trabalho_POO_ED1/main.cpp
@@ -0,0 +1,12 @@
+/* File:   main.cpp
+ * Author: Gabriel Peres
+ * Created on 8 de Maio de 2018, 22:48
+ */
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
