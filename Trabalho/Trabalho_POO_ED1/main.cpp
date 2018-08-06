074443ba6cbdea84c56291270f2c1a05bc2a5aa4
Gabriel Peres <34287414+SirAargon@users.noreply.github.com>
2018-08-06 17:25:25 -0300

    colocando na master

--- Trabalho/Trabalho_POO_ED1/main.cpp
+++ Trabalho/Trabalho_POO_ED1/main.cpp
@@ -14,15 +14,28 @@ int main(int argc, char** argv)
     
     Palavra *p1 = new Palavra();
     Palavra *p2 = new Palavra();
-    p1->setPalavra("atum!");
-    p2->setPalavra("atim!");  
+    Palavra *p3 = new Palavra();
+    Palavra *p4 = new Palavra();
+    Palavra *p5 = new Palavra();
+    Palavra *p6 = new Palavra();
+    Palavra *p7 = new Palavra();
+    Palavra *p8 = new Palavra();
+    p1->setPalavra("g");
+    p2->setPalavra("b");  
+    p3->setPalavra("f");
+    p4->setPalavra("e");
+    p5->setPalavra("z");
+    p6->setPalavra("c");
+    p7->setPalavra("d");
+    p8->setPalavra("t");
+    
     if (*p1>*p2)
     {
         cout<<"atum > atim\n";
     }
     
     if (p1->semelhante(*p2)==1)
-        cout<<"Palavras Semelhantes";
+        cout<<"Palavras Semelhantes" <<endl;
     
     //TEXTO//
     
@@ -39,8 +52,19 @@ int main(int argc, char** argv)
     //CORRETOR//
     
     //ARVORE//
+    Arvore p;
     
     
-    return 0;
-}
+   p.insere1(*p1);
+   p.insere1(*p2);
+   p.insere1(*p3);
+   p.insere1(*p4);
+   p.insere1(*p5);
+   p.insere1(*p6);
+   p.insere1(*p7);
+   p.insere1(*p8);
 
+    p.preordem(p.getRaiz());
+    p.emordem(p.getRaiz());
+
+}
